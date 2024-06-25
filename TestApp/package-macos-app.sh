87f6466ad71a9d8d7c82479f270f47c086f5a3c7
MattEqualsCoder <mattequalscoder@gmail.com>
2024-06-25 19:12:10 -0400

    Initial commit

---
+++ TestApp/package-macos-app.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+dotnet publish -r osx-arm64 --configuration Release -p:UseAppHost=true
+
+# Define variables
+APP_NAME="TestApp.app"
+ZIP_FILE="TestApp.zip"
+PUBLISH_OUTPUT_DIRECTORY="bin/Release/net8.0/osx-arm64/publish"
+INFO_PLIST="Info.plist"
+ICON_FILE="AppIcon.icns"
+
+# Remove old .app bundle if it exists
+if [ -d "$APP_NAME" ]; then
+    rm -rf "$APP_NAME"
+fi
+
+# Create the .app bundle structure
+mkdir -p "$APP_NAME/Contents/MacOS"
+mkdir -p "$APP_NAME/Contents/Resources"
+
+# Copy the Info.plist file and the icon
+cp "$INFO_PLIST" "$APP_NAME/Contents/Info.plist"
+cp "$ICON_FILE" "$APP_NAME/Contents/Resources/AppIcon.icns"
+
+# Copy the published output to the MacOS directory
+cp -a "$PUBLISH_OUTPUT_DIRECTORY/." "$APP_NAME/Contents/MacOS"
+
+echo "Packaged $APP_NAME successfully."
+
+mkdir -p "Setup/output"
+
+# Zip the .app bundle
+# zip -r "Setup/output/$ZIP_FILE" "$APP_NAME"
